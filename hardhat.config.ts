7ab61faa2870c84eca8bcc7f4e54f52404164efd
Sarthak Verma <sarthakvdev@gmail.com>
2022-10-31 03:04:32 +0530

    development env setup using hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+    // polygon": {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: [process.env.pk]
+    // }
+  },
+};
+
+export default config;
