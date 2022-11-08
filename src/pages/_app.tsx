4a6f8342c42654e02770af155c927cc7f45ed625
Sarthak Verma <sarthakvdev@gmail.com>
2022-11-08 16:37:45 +0530

    Using Rainbowkit & WAGMI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,44 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import Link from "next/link";
+import { css } from "@emotion/css";
+import { ethers } from "ethers";
+import { AccountContext } from "src/context";
+import { ownerAddress } from "../../config";
+import "easymde/dist/easymde.min.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import "@rainbow-me/rainbowkit/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [account, setAccount] = useState(null);
+
+  const { chains, provider } = configureChains(
+    [chain.polygonMumbai],
+    [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "BlogThree",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
 export default MyApp;
